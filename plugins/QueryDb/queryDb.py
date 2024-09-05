e0259dfb3dc344d968e04d20243e38da238faee1
tsukabon <shtsukam@outlook.com>
2024-09-05 12:13:35 +0900

    planner をつかった自然言語問い合わせプロトタイプ。first commit.

--- plugins/QueryDb/queryDb.py
+++ plugins/QueryDb/queryDb.py
@@ -1,5 +1,6 @@
-from semantic_kernel.plugin_definition import kernel_function, kernel_function_context_parameter
-from semantic_kernel import KernelContext
+from typing import Annotated,Optional #,List, Optional, TypedDict
+from semantic_kernel.functions import kernel_function #, kernel_function_context_parameter
+#from semantic_kernel import KernelContext
 
 import pyodbc
 
@@ -15,23 +16,25 @@ class QueryDbPlugin:
     def __clean_sql_query__(sql_query):
         sql_query = sql_query.replace(";", "")
         sql_query = sql_query.replace("/n ", " ")
+        sql_query = sql_query.replace("T-SQL:", "")
+        sql_query = sql_query.replace("```sql", "")
+        sql_query = sql_query.replace("```", "")
 
         return sql_query  
     
     @kernel_function(name="query_db",
-                     description="Query a database using a SQL query")
-    @kernel_function_context_parameter(name="input",
-                                       description="SQL Query to be executed")
-    
-    def query_db(self, context: KernelContext) -> str:    
+                     description="SQLクエリを使用してデータベースに問い合わせる")
+    def query_db(self, input: Annotated[str, "実行するSQLクエリ"]) -> Annotated[Optional[list], "結果を得るためにデータベースに問い合わせる。"]:   
         # Connect to the SQL Server database
         conn = pyodbc.connect(self._connection_string)
-
+        print(f"input=☆{self.__clean_sql_query__(input)}☆")
         # Create a cursor object to execute SQL queries
         cursor = conn.cursor()
-        
+        print(f"★★★★★★★★★★★★★★★★★★★★★★★★")
         try:
-            cursor.execute(self.__clean_sql_query__(context["input"]))
+            sql = self.__clean_sql_query__(input)
+            print(f"sql={sql}")
+            cursor.execute(sql)
             #result = cursor.fetchone()
             
             # Get the column names from cursor.description
@@ -44,7 +47,7 @@ class QueryDbPlugin:
             for row in cursor.fetchall():
                 results.append(dict(zip(columns, row)))
             
-            context["result"] = results
+            return results
         except Exception as e:
             return f"Error: {e}"
         cursor.close()
